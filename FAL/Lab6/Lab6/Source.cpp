6569175963b52f16ea4c3b1c0daec770764df71b
dRaco <36478308+d-Raco@users.noreply.github.com>
2022-08-01 12:25:11 +0200

    Update Source.cpp

--- FAL/Lab6/Lab6/Source.cpp
+++ FAL/Lab6/Lab6/Source.cpp
@@ -1,7 +1,7 @@
 ﻿/*
 I14
 joseignaciodg
-Rymond3
+d-Raco
 
 Steps:
 1. We considered implementing 2 pivots (first and last element) and a boolean which checks if the previous element of the sequence is even.
